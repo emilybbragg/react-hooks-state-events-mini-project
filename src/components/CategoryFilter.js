d3a82f667a054215c45cc1f9ffca303afd6f3812
Emily <emilybribragg@gmail.com>
2022-04-20 17:00:53 -0400

    commit tree f4a0a31f1c02154246f4c1f25b3e61300ab63cad

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectCategory}) {
+  const categoryList = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : "";
+    return (
+      <button
+      key={category}
+      className={className}
+      onClick={() => onSelectCategory(category)}
+      >
+        {category}
+      </button>
+    )
+  })
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+        {categoryList}
     </div>
   );
 }
 
 export default CategoryFilter;
+
+
+
+/*
+Pass the list of categories to this component from App
+update this component to display <button> elements for each category (again, make sure to use a key prop).
+*/
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task({text, category, onDeleteTask}) {
+  function handleClick() {
+    onDeleteTask(text)
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
 
 export default Task;
+
+//Update the Task component so that it shows the task's text and category.
